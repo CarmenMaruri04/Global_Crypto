fe1cca1de4d6dadc1ac502eca519ed6ea197bb43
Gemma <gemmalisseth.vaca@estudiant.enti.cat>
2025-12-11 20:41:31 +0100

    git commit -m "GLOB-11: Crear menú principal"
    Se crea el menú principal (en interfície gráfica) en bucle, donde se muestra una introducción describiendo a “GlobalCrypto” y abajo se muestra las 4 opciones (en botones de la interfície gráfica)

---
+++ main.py
@@ -0,0 +1,32 @@
+﻿import tkinter as tk #Importamos la interficie grafica que vamos a usar
+from tkinter import messagebox #Importamos messagebox para el easteregg
+
+def menu_principal(): #Menu principal con todas sus opciones
+    etiqueta_main = tk.Label(root, text = "Bienvenido a GlobalCrypto \nGlobalCrypto es tu puerta de entrada al mundo de la criptografia y la seguridad digital, presentado de una forma sencilla y divertida. \nDesde el menu principal podras acceder a diferentes misiones y retos disenados para que aprendas paso a paso, poniendo en practica tus conocimientos mientras disfrutas de una experiencia interactiva.\nEl programa esta organizado en niveles de dificultad (facil, intermedio y dificil), para que avances a tu ritmo y descubras nuevas opciones a medida que progresas. \nCada mision te plantea situaciones reales y dinamicas, como la Mision en el hospital o el Archivo Fantasma, que te ayudaran a comprender como funciona la criptografia en la vida cotidiana.\nGlobalCrypto no es solo una herramienta de aprendizaje: es un espacio donde podras experimentar, equivocarte sin miedo y mejorar tus habilidades de forma entretenida. \nEl menu principal sera tu punto de partida para explorar todo lo que el programa tiene preparado para ti.")
+    etiqueta_main.pack()
+    boton_nivel_facil = tk.Button(root, text = "Nivel Facil") #Falta comando de clicar opcion
+    boton_nivel_facil.pack()
+    boton_nivel_intermedio = tk.Button(root, text = "Nivel Intermedio") #Falta comando de clicar opcion
+    boton_nivel_intermedio.pack()
+    boton_nivel_dificil = tk.Button(root, text = "Nivel Dificil") #Falta comando de clicar opcion
+    boton_nivel_dificil.pack()
+
+    boton_salir = tk.Button(root, text = "Salir", command = root.destroy) #Cerrar ventana
+    boton_salir.pack()
+
+def easter_egg1():
+    def mensaje():
+        messagebox.showinfo(message = 'GRACIAS POR USAR GLOBALCRYPTO')
+
+    emoji = tk.Button(root, text = "😀", command = mensaje)
+    emoji.pack()
+
+root = tk.Tk() #Crear ventana
+root.title("GlobalCrypto")
+
+menu_principal() #Llamando a menu principal con todas sus opciones
+easter_egg1()
+
+
+
+root.mainloop() #Para que se realice
